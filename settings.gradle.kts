dc3b95fb992ff571029db44da27f49c534c0bfe4
Rafael Costa <rafael@switchpayments.com>
2021-10-17 18:56:28 +0100

    Removed unneeded pluginManagement block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,3 @@
-pluginManagement {
-    plugins {
-        id("com.google.devtools.ksp") version "1.5.30-1.0.0"
-        kotlin("jvm") version "1.5.30"
-    }
-    repositories {
-        gradlePluginPortal()
-        google()
-    }
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
