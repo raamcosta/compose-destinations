61ce0a7c5223390b0252608fef0b8a9e9ef949f7
Rafael Tonholo <rafael@tonholo.dev>
2022-04-06 15:19:42 -0300

    added ktx serializable navtype template

--- compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/templates/NavTypeTemplate.kt
+++ compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/templates/NavTypeTemplate.kt
@@ -103,3 +103,38 @@ class ${NAV_TYPE_CLASS_SIMPLE_NAME}(
         value?.let { encodeForRoute(customSerializer.toRouteString(value)) }
 }
 """.trimIndent()
+
+val ktxSerializableNavTypeTemplate = """
+package $codeGenBasePackageName.navtype
+
+import android.os.Bundle
+import java.io.Serializable
+import $CORE_PACKAGE_NAME.navargs.DestinationsNavType
+import $CORE_PACKAGE_NAME.navargs.ktxserializable.DefaultKtxSerializableNavTypeSerializer
+import $CORE_PACKAGE_NAME.navargs.utils.encodeForRoute$ADDITIONAL_IMPORTS
+import kotlinx.serialization.ExperimentalSerializationApi
+
+@OptIn(ExperimentalSerializationApi::class)
+val $NAV_TYPE_NAME = ${NAV_TYPE_CLASS_SIMPLE_NAME}(
+    $SERIALIZER_SIMPLE_CLASS_NAME
+)
+
+@OptIn(ExperimentalSerializationApi::class)
+class ${NAV_TYPE_CLASS_SIMPLE_NAME}(
+    private val serializer: DefaultKtxSerializableNavTypeSerializer<$CLASS_SIMPLE_NAME_CAMEL_CASE>
+) : DestinationsNavType<${CLASS_SIMPLE_NAME_CAMEL_CASE}?>() {
+
+    override fun get(bundle: Bundle, key: String): ${CLASS_SIMPLE_NAME_CAMEL_CASE}? =
+        bundle.getString(key)?.let { parseValue(it) }
+
+    override fun put(bundle: Bundle, key: String, value: ${CLASS_SIMPLE_NAME_CAMEL_CASE}?) {
+        bundle.putString(key, serializeValue(value))
+    }
+
+    override fun parseValue(value: String): $CLASS_SIMPLE_NAME_CAMEL_CASE =
+        serializer.fromRouteString(value)
+
+    override fun serializeValue(value: ${CLASS_SIMPLE_NAME_CAMEL_CASE}?): String? =
+        value?.let { encodeForRoute(serializer.toRouteString(value)) }
+}
+""".trimIndent()
