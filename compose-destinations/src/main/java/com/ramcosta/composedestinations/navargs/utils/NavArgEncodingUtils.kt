d26339f7632a36348cec1fb6d7bcb4f7db2a4a16
Rafael Tonholo <rafael@tonholo.dev>
2022-04-06 15:18:26 -0300

    removed duplicated code

--- compose-destinations/src/main/java/com/ramcosta/composedestinations/navargs/utils/NavArgEncodingUtils.kt
+++ compose-destinations/src/main/java/com/ramcosta/composedestinations/navargs/utils/NavArgEncodingUtils.kt
@@ -8,13 +8,13 @@ fun encodeForRoute(arg: String): String {
     return Uri.encode(arg)
 }
 
-internal fun String.base64ToByteArray(): ByteArray {
+fun String.base64ToByteArray(): ByteArray {
     return Base64.decode(
         toByteArray(Charset.defaultCharset()),
         Base64.URL_SAFE or Base64.NO_WRAP
     )
 }
 
-internal fun ByteArray.toBase64Str(): String {
+fun ByteArray.toBase64Str(): String {
     return Base64.encodeToString(this, Base64.URL_SAFE or Base64.NO_WRAP)
 }
