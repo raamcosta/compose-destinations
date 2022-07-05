3046ba4b6a7263384c83ddccfef9d0ba268ad7d5
Rafael Costa <rafael.alexm.costa@gmail.com>
2022-07-05 23:11:32 +0100

    Added kdocs to NavHostParam annotation

--- compose-destinations/src/main/java/com/ramcosta/composedestinations/annotation/NavHostParam.kt
+++ compose-destinations/src/main/java/com/ramcosta/composedestinations/annotation/NavHostParam.kt
@@ -1,7 +1,21 @@
 package com.ramcosta.composedestinations.annotation
 
 /**
+ * Annotation that can be used in arguments of Destination Composable
+ * functions to signal Compose Destinations to not consider it as a
+ * navigation argument.
+ * By default, all arguments with types Compose Destinations allows
+ * you to pass as a navigation argument are considered navigation
+ * arguments. So use this if you have an argument with such type
+ * but you don't want to pass it from previous destination.
  *
+ * There are usually two types of arguments you can set on Destination
+ * annotated functions:
+ * - Navigation arguments
+ * - Parameters passed in from DestinationsNavHost level
+ *
+ * Arguments marked with this annotation, need to be passed in from
+ * DestinationsNavHost.
  */
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.SOURCE)
