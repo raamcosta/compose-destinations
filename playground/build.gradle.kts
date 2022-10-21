e351232e4343d7e01f21b84a1e9048aec7fc6770
Martin Jantošovič <jantosovic.martin@gmail.com>
2022-10-21 22:42:13 +0200

    feat(tests): Added failing tests navArgsDelegate

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -107,4 +107,8 @@ dependencies {
 
     testImplementation(libs.test.junit)
     testImplementation(libs.test.mockk)
+
+    testImplementation(project(":compose-destinations-ksp"))
+    testImplementation(libs.test.kotlinCompile)
+    testImplementation(libs.test.kotlinCompileKsp)
 }
