1b13b88fa03be85b6f68e17ae3c74725d2352cc8
Maxime Michel <maxime.2.michel@worldline.com>
2022-10-25 14:27:59 +0200

    :memo: Add documentation

--- compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/model/Core.kt
+++ compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/model/Core.kt
@@ -1,5 +1,11 @@
 package com.ramcosta.composedestinations.codegen.model
 
+/**
+ * The different available core modules for the library.
+ *
+ * [MAIN] is the basic module with most of the implementations whereas [ANIMATIONS] is the module
+ * containing the animation implementations (Based on the Accompanist library).
+ */
 enum class Core {
     MAIN, ANIMATIONS
 }
