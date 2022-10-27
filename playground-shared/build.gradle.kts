ae6f34d572baed2e40fb8bbc93ed5b97763a73a9
Martin Jantošovič <jantosovic.martin@gmail.com>
2022-10-27 10:50:10 +0200

    fix(ksp): Add more meaningful error message when KSP cannot detect default value for navArgsDelegate

---
+++ playground-shared/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("java-library")
+    id("kotlin")
+    kotlin("plugin.serialization")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
