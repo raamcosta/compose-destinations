660ce6ba1f94e47a7400fbe93cd5f0e47ab769b9
Rafael Costa <rafael.alexm.costa@gmail.com>
2023-02-26 17:53:22 +0000

    set kotlin jvmTarget to 1.8

--- compose-destinations-codegen/build.gradle.kts
+++ compose-destinations-codegen/build.gradle.kts
@@ -9,3 +9,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
--- compose-destinations-ksp/build.gradle.kts
+++ compose-destinations-ksp/build.gradle.kts
@@ -4,8 +4,17 @@ plugins {
 
 apply(from = "${rootProject.projectDir}/publish.gradle")
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
