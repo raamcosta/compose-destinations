0f31aba9c050fe6eb74f12166d7f815115c43e51
Rafael Costa <rafael.alexm.costa@gmail.com>
2023-06-03 17:55:14 +0100

    removed legacy nav graph definition apis

--- compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/model/NavGraphInfo.kt
+++ compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/model/NavGraphInfo.kt
@@ -1,23 +1,9 @@
 package com.ramcosta.composedestinations.codegen.model
 
-sealed interface NavGraphInfo {
-
-    val start: Boolean
-    val isDefault: Boolean
-
-    data class Legacy(
-        override val start: Boolean,
-        val navGraphRoute: String
-    ): NavGraphInfo {
-        override val isDefault: Boolean
-            get() = !start && navGraphRoute == "root"
-    }
-
-    data class AnnotatedSource(
-        override val start: Boolean,
-        val isNavHostGraph: Boolean,
-        val graphType: Importable
-    ): NavGraphInfo {
-        override val isDefault = false
-    }
+data class NavGraphInfo(
+    val start: Boolean,
+    val isNavHostGraph: Boolean,
+    val graphType: Importable
+) {
+    val isDefault = false
 }
