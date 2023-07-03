5f31422798bcdd32f6e6926de6421c939838d808
Rafael Costa <rafael.alexm.costa@gmail.com>
2023-07-03 22:24:30 +0100

    add NavGraphs object visual kdocs

--- compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/templates/NavGraphsObjectTemplate.kt
+++ compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/templates/NavGraphsObjectTemplate.kt
@@ -9,6 +9,7 @@ import com.ramcosta.composedestinations.codegen.templates.core.FileTemplate
 import com.ramcosta.composedestinations.codegen.templates.core.setOfImportable
 
 const val NAV_GRAPHS_PLACEHOLDER = "[NAV_GRAPHS_PLACEHOLDER]"
+const val NAV_GRAPHS_PRETTY_KDOC_PLACEHOLDER = "[NAV_GRAPHS_PRETTY_KDOC_PLACEHOLDER]"
 
 val navGraphsObjectTemplate = FileTemplate(
     packageStatement = "package $codeGenBasePackageName",
@@ -20,7 +21,7 @@ val navGraphsObjectTemplate = FileTemplate(
     sourceCode = """
 /**
  * Class generated if any Composable is annotated with `@Destination`.
- * It aggregates all [$CORE_ALIAS_DESTINATION_SPEC]s in their [$CORE_ALIAS_NAV_GRAPH_SPEC]s.
+ * It aggregates all [$CORE_ALIAS_DESTINATION_SPEC]s in their [$CORE_ALIAS_NAV_GRAPH_SPEC]s.$NAV_GRAPHS_PRETTY_KDOC_PLACEHOLDER
  */
 internal object $GENERATED_NAV_GRAPHS_OBJECT {
 
