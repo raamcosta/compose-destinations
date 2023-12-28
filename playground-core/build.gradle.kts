560eef5b497271accc501ff0d10e0e80bb22766f
Rafael Costa <rafael.alexm.costa@gmail.com>
2023-12-28 02:25:39 +0000

    re-enabled tests

--- playground-core/build.gradle.kts
+++ playground-core/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
