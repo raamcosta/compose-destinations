3d9764d77c2d91f998368fbada1e17ab64b73bbf
Rafael Costa <rafael.alexm.costa@gmail.com>
2024-07-25 12:23:48 +0100

    Fixes #668

--- compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/model/DefaultValue.kt
+++ compose-destinations-codegen/src/main/java/com/ramcosta/composedestinations/codegen/model/DefaultValue.kt
@@ -1,6 +1,21 @@
 package com.ramcosta.composedestinations.codegen.model
 
-data class DefaultValue(
-    val code: String,
-    val imports: List<String> = emptyList()
-)
+import com.ramcosta.composedestinations.codegen.commons.IllegalDestinationsSetup
+
+sealed interface DefaultValue {
+
+    data class Error(val throwable: Throwable) : DefaultValue
+
+    data object NonExistent: DefaultValue
+
+    data class Available(
+        val code: String,
+        val imports: List<String> = emptyList()
+    ) : DefaultValue
+}
+
+fun DefaultValue.unwrapAvailable(): DefaultValue.Available? = when (this) {
+    is DefaultValue.Available -> this
+    is DefaultValue.Error -> throw IllegalDestinationsSetup("Error reading default value", throwable)
+    is DefaultValue.NonExistent -> null
+}
